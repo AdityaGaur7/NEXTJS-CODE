7b0295d6f64c70ac3cec75904617275391199e22
AdityaGaur7 <adgaur027@gmail.com>
2024-06-27 14:42:59 +0530

    navigation links

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,30 @@
 "use client";
 
-import { useState } from "react";
-
+import { use, useState } from "react";
+import Link from "next/link";
+import {useRouter} from 'next/navigation'
 export default function Home() {
-  let num = 1;
-  const [ok,setok]=useState(0);
-  const fruit = () => {
-    // alert(item);
-  num++;
-  
-    setok(num);
-  };
 
-
-  const Inner = ()=>{
-    return <h1>inner</h1>
+  const router = useRouter();
+  const navigate=(params)=>{
+    router.push(params)
   }
   return (
     <div>
       <h1>
         Basic routing
       </h1>
+      <Link href="login">login</Link>
+      <br />   <Link href="logout">logout</Link>
+      <br />   <Link href="about">about</Link>
+ <br />
+      <button onClick={()=>navigate("/login")}>
+        go to login
+      </button>
+      <button onClick={()=>navigate("/logout")}>
+        go to logout
+      </button>
+      <br />
     </div>
   );
 }
