b7177f2a74f2cfda4ed3579e87e3f1fe4b4249d2
AdityaGaur7 <adgaur027@gmail.com>
2024-06-27 18:44:11 +0530

    page not found

---
+++ src/app/about/[...not-found]/page.js
@@ -0,0 +1,7 @@
+export default function Error() {
+    return (
+        <h2>
+       Nikl lawde nhi zarurat hai teri 
+        </h2>
+    )
+}
---
+++ src/app/not-found.js
@@ -0,0 +1,7 @@
+export default function Error() {
+    return (
+        <h2>
+            chla ja bsdk
+        </h2>
+    )
+}
