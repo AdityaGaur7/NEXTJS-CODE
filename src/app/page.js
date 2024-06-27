d2e708f2a74e1c7164a78675ab3a196c1b2f484c
AdityaGaur7 <adgaur027@gmail.com>
2024-06-27 13:56:09 +0530

    event state function

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,27 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  let num = 1;
+  const [ok,setok]=useState(0);
+  const fruit = () => {
+    // alert(item);
+  num++;
+  
+    setok(num);
+  };
+
+
+  const Inner = ()=>{
+    return <h1>inner</h1>
+  }
   return (
-   <h1>
-    <User name="Adi"/>
-    <User name="bhumi"/>
-    <User name="alok"/>
-    home
-   </h1>
+    <div>
+      <h1>events , function , state {ok}</h1>
+      <button onClick={()=>fruit()}>click</button>
+      <Inner/>
+      {Inner()}
+    </div>
   );
 }
-
-const User=(props)=>{
-  return (
-    <h2>
-      hello there {props.name}
-    </h2>
-  )
-};
-
