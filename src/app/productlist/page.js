bd319694f9242152089d016966f54e66bed907cf
AdityaGaur7 <adgaur027@gmail.com>
2024-06-28 09:50:17 +0530

    css module

--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -1,6 +1,7 @@
 "use client"
 import{ useEffect, useState } from "react";
-
+import custom from "./custom.module.css"
+import outside from "@/app/style/outside.module.css"
 export default function Product() {
     const [product,setproduct]=useState([]);
     useEffect(() => {
@@ -20,13 +21,13 @@ export default function Product() {
 
     return (
         <div>
-            <h2>Fetch data from API in client component</h2>
-            <h3>
+            <h2 className={custom.h1}>Fetch data from API in client component</h2>
+            <h3 className={custom.h3}>
                 Product list
             </h3>
             {
                 product.map((item,index)=>(
-                    <div key={index}>
+                    <div key={index} className={outside.h2}>
                         <p>Name:{item.title}</p>
                         <p>Price:{item.price}</p>
                         <p>Description{item.description}</p>
