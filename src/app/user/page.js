8f5ba964a208f66803268e17a6a249873abe82c9
AdityaGaur7 <adgaur027@gmail.com>
2024-06-28 10:48:45 +0530

    scripting

---
+++ src/app/user/page.js
@@ -0,0 +1,17 @@
+"use client"
+
+import Script from "next/script"
+
+
+export default function Location() {
+    return (
+        <div>
+            <Script
+            src="/script.js"
+            />
+                
+                Location
+
+        </div>
+    )
+}
