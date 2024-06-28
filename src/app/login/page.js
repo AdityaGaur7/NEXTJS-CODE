1924d65570bc74a0f5f274be9dd99c8ed088e44f
AdityaGaur7 <adgaur027@gmail.com>
2024-06-28 10:13:37 +0530

    image optimization

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,3 +1,22 @@
+"use client"
+import Image from "next/image"
+import Profile from "../../../public/vercel.svg"
 export default function Login() {
-    return (<h1>login</h1>)
+    console.log(Profile)
+    return (
+<div>
+
+
+        <h1>login</h1>
+<Image
+src={Profile}
+
+/>
+<Image
+src="https://adgaurportfolio.vercel.app/images/programmer.webp"
+width={400}
+height={500}
+/>
+</div>
+)
 };
