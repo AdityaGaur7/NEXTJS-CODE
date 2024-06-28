833b1053f9441bd2d44803b203333527dbd04bab
AdityaGaur7 <adgaur027@gmail.com>
2024-06-28 10:32:01 +0530

    generateMetadata for dynamic metadata

--- src/app/logout/page.js
+++ src/app/logout/page.js
@@ -1,3 +1,11 @@
 export default function Logout() {
     return (<h1>logout</h1>)
 };
+export function generateMetadata(){
+    return {
+        title:"logout page",
+        description:"logout page",
+        keywords:"logout",
+        image:"https://adgaurportfolio.vercel.app/images/programmer.webp"
+    }
+}
