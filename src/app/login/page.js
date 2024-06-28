a72282c7ad3197d0678aeae2d4a2c20ad7d75360
AdityaGaur7 <adgaur027@gmail.com>
2024-06-28 10:20:24 +0530

    font optimization

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,13 +1,20 @@
 "use client"
 import Image from "next/image"
 import Profile from "../../../public/vercel.svg"
+import { Roboto } from "next/font/google"
+
+const roboto = Roboto({
+    weight:'100',
+    subsets:['latin'],
+    display:'swap'
+})
 export default function Login() {
     console.log(Profile)
     return (
 <div>
 
 
-        <h1>login</h1>
+        <h1 className={roboto.className}>login</h1>
 <Image
 src={Profile}
 
