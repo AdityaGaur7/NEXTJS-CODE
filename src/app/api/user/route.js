784a7855923a7f229127af6894aa26731cf2b4c3
AdityaGaur7 <adgaur027@gmail.com>
2024-06-29 16:33:07 +0530

    make api with POST method

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -5,3 +5,13 @@ export async function GET(request){
     const data = user;
     return NextResponse.json(data,{status:200});
 }
+
+export async function POST(req,res){
+    
+    let payload = await req.json();
+    console.log(payload);
+    if(!payload.name){
+    return NextResponse.json({result:"require data not found",sucess:false},{status:404});
+    }
+    return NextResponse.json({name:"hello",success:true},{status:201});
+}
