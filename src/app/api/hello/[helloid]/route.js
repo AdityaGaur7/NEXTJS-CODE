aa7cc5afb904469dea75f4dcc0ed7edf83e974de
AdityaGaur7 <adgaur027@gmail.com>
2024-07-01 12:01:52 +0530

    PUT API WITH MONGODB

--- src/app/api/hello/[helloid]/route.js
+++ src/app/api/hello/[helloid]/route.js
@@ -1,11 +1,16 @@
+import { Product } from "@/app/lib/model/product";
 import { NextResponse } from "next/server";
-
+import mongoose from "mongoose";
+const url = process.env.MONGO;
 export async function PUT(req,res){
-    // const payload =await req.json();
-    // await mongoose.connect(url);
-    // let product = new Product(payload);
-    // const res  = await product.save();
-    console.log(res);
+  
+    
+    const index = res.params.helloid;
+    const filter ={_id:index};
+    const payload = await req.json();
+    console.log(payload);
+    await mongoose.connect(url);
+    const data = await Product.findOneAndUpdate(filter,payload);
 
-    return NextResponse.json({result:res,success:true});
+    return NextResponse.json({result:data,success:true});
 }
